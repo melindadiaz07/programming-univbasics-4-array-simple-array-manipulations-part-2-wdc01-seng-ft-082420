7046c9ad5ef208d49dbc873c42bc0a8c105104cd
Melinda Diaz <melindadiaz@me.com>
2020-07-19 01:57:45 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,3 +1,3 @@
-def using_concact(my_favorite_things,more_favs)
-  all_my_favs = my_favorite_things.concact(more_favs)
+def using_concact(my_favorite_things, more_favs)
+   my_favorite_things.concact(more_favs)
 end 
